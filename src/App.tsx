a0bb1e2a2eb2dbdecd9f3bb1134c7647eea2dfb9
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-14 18:50:47 +0900

    feat: 메인 페이지 스켈레톤 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Routes, Route} from "react-router-dom";
-import SignInPage from "./pages/signin/page/SignInPage";
-import SignUpPage from "./pages/signup/page/SignUpPage";
+import SignInPage from "./pages/signin/SignInPage";
+import SignUpPage from "./pages/signup/SignUpPage";
+import MainPage from "./pages/main/MainPage";
 
 const App: React.FC = () => (
       <Routes>
           <Route path="/" element={<SignInPage />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/main" element={<MainPage />} />
       </Routes>
 );
 
---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import TopNavBar from "../../components/common/TopNavBar";
+import Balance from "./component/Balance";
+
+import { IoIosArrowForward } from "react-icons/io";
+import { IoSearch } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxDotsHorizontal } from "react-icons/rx";
+import { IoHeartOutline } from "react-icons/io5";
+import { IoHeartSharp } from "react-icons/io5";
+import { FaArrowLeft } from "react-icons/fa6";
+
+const MainPage: React.FC = () => {
+    return (
+        <div>
+            <TopNavBar
+                types={{
+                    Left: FaArrowLeft,
+                    RightFirst: IoHeartSharp,
+                    RightSecond: IoHeartOutline,
+                    RightThird: RxHamburgerMenu
+                }}
+                components={{
+                    Left: {},
+                    RightFirst: {},
+                    RightSecond: {},
+                    RightThird: {}
+                }}
+            />
+
+            <Balance/>
+
+            <div className={"flex items-center text-xl p-4"}>
+                주문내역
+                <IoIosArrowForward/>
+            </div>
+
+        </div>
+    )
+}
+
+export default MainPage;

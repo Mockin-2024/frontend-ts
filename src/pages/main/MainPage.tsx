fa58504f9074150ed685ff0d523c8d5fe6906e3c
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-14 21:25:04 +0900

    refactor: Mainpage 리팩토링
    TopNavBar 리팩토링한 사항 적용해
    코드 개선

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,41 +1,19 @@
 import React from "react";
 
-import TopNavBar from "../../components/common/TopNavBar";
 import Balance from "./components/Balance";
 
 import { IoIosArrowForward } from "react-icons/io";
-import { IoSearch } from "react-icons/io5";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { RxDotsHorizontal } from "react-icons/rx";
-import { IoHeartOutline } from "react-icons/io5";
-import { IoHeartSharp } from "react-icons/io5";
-import { FaArrowLeft } from "react-icons/fa6";
+import MainTopNavBar from "./components/MainTopNavBar";
 
 const MainPage: React.FC = () => {
     return (
         <div>
-            <TopNavBar
-                types={{
-                    Left: FaArrowLeft,
-                    RightFirst: IoHeartSharp,
-                    RightSecond: IoHeartOutline,
-                    RightThird: RxHamburgerMenu
-                }}
-                components={{
-                    Left: {},
-                    RightFirst: {},
-                    RightSecond: {},
-                    RightThird: {}
-                }}
-            />
-
+            <MainTopNavBar />
             <Balance/>
-
             <div className={"flex items-center text-xl p-4"}>
                 주문내역
                 <IoIosArrowForward/>
             </div>
-
         </div>
     )
 }

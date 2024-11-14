9bfd00beea5360fbafae41d2b69cd4730bdc9ae7
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-14 18:50:15 +0900

    feat: 자산 목록 컴포넌트 추가
    훅은 나중에 적용할 예정

---
+++ src/pages/main/component/Balance.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Balance = () =>{
+    return(
+        <div className={"pt-16 p-4"}>
+            <div className={""}>
+                내 투자
+            </div>
+
+            <div className={"text-3xl"}>
+                {}원
+            </div>
+            <div>
+                분류 기준
+            </div>
+            <div className={"flex justify-between"}>
+                <div className={"text-xl "}>
+                    나스닥
+                </div>
+                <div className={"flex-col text-xl"}>
+                    <div>
+                        {}원
+                    </div>
+                    <div>
+                        {}({}%)
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Balance

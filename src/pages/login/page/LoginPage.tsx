0ca18b8d2003b3bb15e8231ab58e6e3ca9efc97a
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-14 15:48:19 +0900

    refactor: 로그인 페이지 개선
    Button Component 활용하고,
    커스텀 훅 활용

--- src/pages/login/page/LoginPage.tsx
+++ src/pages/login/page/LoginPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import InputField from "../../../components/InputField";
+import Button from "../../../components/Button";
+import useSignInClickHandler from "../hooks/useSignInClickHandler";
 
 const LoginPage: React.FC = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const buttonOnClick = useSignInClickHandler();
 
     return (
         <div className={"flex items-center justify-center h-screen flex-col"}>
@@ -16,20 +19,20 @@ const LoginPage: React.FC = () => {
                 type={"text"}
                 placeholder={"email"}
                 value={email}
-                onChange={e => setEmail(e.target.value)} />
+                onChange={e => setEmail(e.target.value)}
+            />
 
             <InputField
                 type={"password"}
                 placeholder={"password"}
                 value={password}
-                onChange={e => setPassword(e.target.value)} />
+                onChange={e => setPassword(e.target.value)}
+            />
 
-            <div className={"flex p-2 justify-center w-full"}>
-                <button
-                className={"p-4 bg-indigo-600 text-white text-2xl outline-none rounded-3xl border-none w-3/4"}>
-                    Sign In
-                </button>
-            </div>
+            <Button
+            value={"Sign In"}
+            onClick={buttonOnClick}
+            />
         </div>
     )
 }

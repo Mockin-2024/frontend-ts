1711b9088d91dbed77a39e1a954cef8ea145957c
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-14 21:45:53 +0900

    style: TopNavBar 스타일 개선
    아이콘을 더 크게
    간격을 넓혀서 답답하지 않게
    만듦.

--- src/components/common/TopNavBar.tsx
+++ src/components/common/TopNavBar.tsx
@@ -10,9 +10,9 @@ interface Props {
 
 const TopNavBar:React.FC<Props> = ({Left, RightFirst, RightSecond, RightThird}) => {
     return (
-        <div className={"fixed top-0 left-0 right-0 z-10 flex justify-between items-center p-4 border-none text-2xl text-white bg-indigo-600"}>
+        <div className={"fixed top-0 left-0 right-0 z-10 flex justify-between items-center p-4 border-none text-3xl text-white bg-indigo-600"}>
             {Left ? <Left /> : <div className="w-16"/>}
-            <div className="flex space-x-2">
+            <div className="flex space-x-4">
                 {RightFirst ? <RightFirst/> : <div className="w-16"/>}
                 {RightSecond  ? <RightSecond /> : <div className="w-16"/>}
                 {RightThird  ? <RightThird /> : <div className="w-16"/>}

a79953424be282816eadb7dad120764969f01138
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-14 16:32:14 +0900

    feat: App 라우팅에 SignUpPage 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import SignInPage from "./pages/signin/page/SignInPage";
+import SignUpPage from "./pages/signup/page/SignUpPage";
 
 const App: React.FC = () => (
       <Routes>
-        <Route path="/" element={<SignInPage />} />
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
       </Routes>
 );
 

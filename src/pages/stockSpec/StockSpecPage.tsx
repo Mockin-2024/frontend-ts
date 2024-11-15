406593ac64277906f7e5b19466eadf24717086dc
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-15 22:04:49 +0900

    feat: 종목 상세에서 매매 창 이동 추가

--- src/pages/stockSpec/StockSpecPage.tsx
+++ src/pages/stockSpec/StockSpecPage.tsx
@@ -2,17 +2,28 @@ import React, {useState} from 'react'
 import {useLocation} from "react-router-dom";
 import StockSpecTopNavBar from "./components/StockSpecTopNavBar";
 import StockInfo from "./components/StockInfo";
-import StockChart from "./components/StockChart";
 import AskingPrice from "./components/AskingPrice";
 import StockDetailInfo from "./components/StockDetailInfo";
+import StockSpecBottomNavBar from "./components/StockSpecBottomNavBar";
+import useNavigator from "../../hooks/useNavigator";
+import {AppRoutes} from "../../enums/AppRoutes";
 
 const StockSpecPage: React.FC = () => {
     const {stockType, stockId} = useLocation().state || {};
     const [selectedOption, setSelectedOption] = useState<string | null>(null);
+    const {navigateTo} = useNavigator()
 
     const handleOptionSelect = (option: string) => {
         setSelectedOption(option);
     };
+
+    const handleOrderSelect = (orderType: string) => {
+        navigateTo(AppRoutes.ORDER, {
+            orderType: orderType,
+            stockId: stockId,
+            price: "100000"
+        })
+    }
 
     return (
         <div className={""}>
@@ -20,24 +31,26 @@ const StockSpecPage: React.FC = () => {
 
             <div className={"pt-20 p-4"}>
                 <StockInfo name={"나스닥"} price={"100000"} type={"NDAQ"}/>
+                <div className={"flex p-4 justify-between "}>
+                    <div onClick={() => handleOptionSelect('chart')}>차트</div>
+                    <div onClick={() => handleOptionSelect('askingPrice')}>호가</div>
+                    <div onClick={() => handleOptionSelect('detailInfo')}>종목 정보</div>
+                </div>
+
+                <div className={"p-4"}>
+                    {/*{selectedOption === 'chart' && </>}*/}
+                    {selectedOption === 'askingPrice' && <AskingPrice stockId={stockId} stockType={stockType} />}
+                    {selectedOption === 'detailInfo' && <StockDetailInfo/>}
+                </div>
+
+                {/*<div className={"flex p-4 w-full"}>*/}
+                {/*    <StockChart data={stockData}/>*/}
+                {/*</div>*/}
             </div>
 
-            <div className={"flex p-4 justify-between "}>
-                <div onClick={() => handleOptionSelect('chart')}>차트</div>
-                <div onClick={() => handleOptionSelect('askingPrice')}>호가</div>
-                <div onClick={() => handleOptionSelect('detailInfo')}>종목 정보</div>
-            </div>
-
-            <div className={"p-4"}>
-                {/*{selectedOption === 'chart' && </>}*/}
-                {selectedOption === 'askingPrice' && <AskingPrice stockId={stockId} stockType={stockType} />}
-                {selectedOption === 'detailInfo' && <StockDetailInfo/>}
-            </div>
-
-            {/*<div className={"flex p-4 w-full"}>*/}
-            {/*    <StockChart data={stockData}/>*/}
-            {/*</div>*/}
+            <StockSpecBottomNavBar handleOrderSelect={handleOrderSelect}/>
         </div>
+
     )
 }
 

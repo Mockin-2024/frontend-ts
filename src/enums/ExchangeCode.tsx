8b206242fd19a60e93494a759695d3245a9e8ea6
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-15 17:46:32 +0900

    feat: 주식 매매에 필요한 enum 추가

---
+++ src/enums/ExchangeCode.tsx
@@ -0,0 +1,11 @@
+export const enum ExchangeCode {
+    NASDAQ = "NASD",        // 나스닥
+    NYSE = "NYSE",          // 뉴욕
+    AMEX = "AMEX",          // 아멕스
+    SEHK = "SEHK",          // 홍콩
+    SHAA = "SHAA",          // 중국상해
+    SZAA = "SZAA",          // 중국심천
+    TKSE = "TKSE",          // 일본
+    HASE = "HASE",          // 베트남 하노이
+    VNSE = "VNSE"           // 베트남 호치민
+}
---
+++ src/enums/StockTrading.tsx
@@ -0,0 +1,14 @@
+export const enum StockTrading {
+    US_Buy = "VTTT1002U",   // 미국 매수 주문
+    US_Sell = "VTTT1001U",  // 미국 매도 주문
+    JP_Buy = "VTTS0308U",   // 일본 매수 주문
+    JP_Sell = "VTTS0307U",  // 일본 매도 주문
+    CN_SH_Buy = "VTTS0202U",// 상해 매수 주문
+    CN_SH_Sell = "VTTS1005U",// 상해 매도 주문
+    HK_Buy = "VTTS1002U",   // 홍콩 매수 주문
+    HK_Sell = "VTTS1001U",  // 홍콩 매도 주문
+    CN_SZ_Buy = "VTTS0305U",// 심천 매수 주문
+    CN_SZ_Sell = "VTTS0304U",// 심천 매도 주문
+    VN_Buy = "VTTS0311U",   // 베트남 매수 주문
+    VN_Sell = "VTTS0310U"   // 베트남 매도 주문
+}

0836a10d6d87a2642bf3cab44bdf67eb997068f2
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-16 11:14:45 +0900

    feat: App.tsx SearchPage 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+
 import {Routes, Route} from "react-router-dom";
+import {AppRoutes} from "./enums/AppRoutes";
+
 import SignInPage from "./pages/signin/SignInPage";
 import SignUpPage from "./pages/signup/SignUpPage";
 import MainPage from "./pages/main/MainPage";
 import StockSpecPage from "./pages/stockSpec/StockSpecPage";
 import OrderPage from "./pages/order/OrderPage";
-import {AppRoutes} from "./enums/AppRoutes";
+import SearchPage from "./pages/search/SearchPage";
 
 const App: React.FC = () => (
       <Routes>
@@ -15,6 +18,7 @@ const App: React.FC = () => (
           <Route path={AppRoutes.MAIN} element={<MainPage />} />
           <Route path={AppRoutes.STOCK} element={<StockSpecPage />} />
           <Route path={AppRoutes.ORDER} element={<OrderPage />} />
+          <Route path={AppRoutes.SEARCH} element={<SearchPage />} />
       </Routes>
 );
 

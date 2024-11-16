be703bd4e4f284f948ef635e0a628249c78087c8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-16 14:30:49 +0900

    feat: 주문 내역 페이지 스켈레톤 구성

--- src/pages/history/HistoryPage.tsx
+++ src/pages/history/HistoryPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import HistoryTopNavBar from "./components/HistoryTopNavBar";
+import HistoryList from "./components/HistoryList";
 
 const HistoryPage = () => {
     return (
         <div>
-
+            <HistoryTopNavBar/>
+            <div className={"pt-16"}>
+                <HistoryList/>
+            </div>
         </div>
     )
 }

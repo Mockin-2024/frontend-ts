d3600b7f64accb88ff3b7ef8a52a23629c632dc2
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-18 20:53:41 +0900

    feat: StockType enum 추가

---
+++ src/enums/StockType.tsx
@@ -0,0 +1,13 @@
+export const enum StockType {
+    NASDAQ = "512",       // 미국 나스닥
+    NYSE = "513",         // 미국 뉴욕
+    AMEX = "529",         // 미국 아멕스
+    JAPAN = "515",        // 일본
+    HONG_KONG = "501",    // 홍콩
+    HONG_KONG_CNY = "543",// 홍콩 CNY
+    HONG_KONG_USD = "558",// 홍콩 USD
+    VIETNAM_HANOI = "507",// 베트남 하노이
+    VIETNAM_HO_CHI_MINH = "508", // 베트남 호치민
+    SHANGHAI_A = "551",   // 중국 상해 A
+    SHENZHEN_A = "552"    // 중국 심천 A
+}

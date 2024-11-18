6adbce5a103ae7d330839ce830c2d9c1b49473ef
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-18 09:23:04 +0900

    feat: 자동 로그인 적용

--- src/pages/signin/SignInPage.tsx
+++ src/pages/signin/SignInPage.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import { useState } from "react";
+import React, {useState} from "react";
 import InputField from "../../components/common/InputField";
 import Button from "../../components/common/Button";
 import useSignInClickHandler from "./hooks/useSignInClickHandler";
-import {Link} from "react-router-dom";
+import useAutoSignIn from "./hooks/useAutoSignIn";
+import useNavigator from "../../hooks/useNavigator";
+import {AppRoutes} from "../../enums/AppRoutes";
 
 const SignInPage: React.FC = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const {handleClick} = useSignInClickHandler(email, password);
+    const {navigateTo} = useNavigator()
 
+    useAutoSignIn()
     return (
         <div className={"flex items-center justify-center h-screen flex-col"}>
             <div className={"pb-10 text-indigo-600 text-4xl font-bold"}>
@@ -35,8 +38,9 @@ const SignInPage: React.FC = () => {
             onClick={handleClick}
             />
 
-            <div className={"flex p-2 text-indigo-400 text-xl"}>
-                <Link to={"/signup"} >회원가입</Link>
+            <div className={"flex p-2 text-indigo-400 text-xl"}
+            onClick={(e) => navigateTo(AppRoutes.SIGN_UP)}>
+                회원가입
             </div>
         </div>
     )

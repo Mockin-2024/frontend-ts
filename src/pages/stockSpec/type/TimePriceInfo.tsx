1ba5b260261a0dd39666ff075ae8a99692238cd7
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-19 16:40:18 +0900

    feat: 분봉 차트 조회를 위한 인터페이스 추가

---
+++ src/pages/stockSpec/type/TimePriceInfo.tsx
@@ -0,0 +1,16 @@
+export interface TimePriceInfo{
+    // 장시작한국시간
+    sktm: string;
+
+    // 장종료한국시간
+    ektm: string;
+
+    // 다음가능여부
+    next: string;
+
+    // 추가데이타여부
+    more: string;
+
+    // 레코드갯수
+    nrec: string;
+}

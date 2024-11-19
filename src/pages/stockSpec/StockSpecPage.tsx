0d4f15befcd326c3c96a4384c7014e45472064a5
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-19 10:00:44 +0900

    refactor: 주식 상세 페이지, api 로딩 및 에러 핸들링 적용

--- src/pages/stockSpec/StockSpecPage.tsx
+++ src/pages/stockSpec/StockSpecPage.tsx
@@ -9,33 +9,37 @@ import StockSpecBottomNavBar from "./components/StockSpecBottomNavBar";
 import {useLocation} from "react-router-dom";
 import useSelect from "./hooks/useSelect";
 import useSearchInfo from "./hooks/useSearchInfo";
+import LoadingOrError from "../../components/common/LoadingOrError";
 
 const StockSpecPage: React.FC = () => {
     const {stockType, stockId} = useLocation().state || {};
     const {option, handleOptionSelect, handleOrderSelect} = useSelect(stockId)
-    const {searchInfo} = useSearchInfo(stockType, stockId)
+    const { searchInfo, loading: loadingSearchInfo, error: errorSearchInfo } = useSearchInfo(stockType, stockId);
+
+    const isLoading = loadingSearchInfo;
+    const isError = errorSearchInfo;
 
     return (
         <div className={""}>
+            <LoadingOrError loading={isLoading} error={isError} />
             <StockSpecTopNavBar/>
 
             <div className={"pt-20 p-4"}>
-                <StockInfo name={searchInfo.prdt_name} price={"100000"} type={searchInfo.ovrs_excg_name}/>
-                <div className={"flex p-4 justify-between "}>
-                    <div onClick={() => handleOptionSelect('chart')}>차트</div>
-                    <div onClick={() => handleOptionSelect('askingPrice')}>호가</div>
-                    <div onClick={() => handleOptionSelect('detailInfo')}>종목 정보</div>
-                </div>
+                {!isLoading && !isError && searchInfo && (
+                    <>
+                        <StockInfo name={searchInfo.prdt_name} price={"100000"} type={searchInfo.ovrs_excg_name}/>
+                        <div className={"flex p-4 justify-between "}>
+                            <div onClick={() => handleOptionSelect('chart')}>차트</div>
+                            <div onClick={() => handleOptionSelect('askingPrice')}>호가</div>
+                            <div onClick={() => handleOptionSelect('detailInfo')}>종목 정보</div>
+                        </div>
 
-                <div className={"p-4"}>
-                    {/*{selected === 'chart' && </>}*/}
-                    {option === 'askingPrice' && <AskingPrice stockId={stockId} stockType={stockType} />}
-                    {option === 'detailInfo' && <StockDetailInfo/>}
-                </div>
-
-                {/*<div className={"flex p-4 w-full"}>*/}
-                {/*    <StockChart data={stockData}/>*/}
-                {/*</div>*/}
+                        <div className={"p-4"}>
+                            {option === 'askingPrice' && <AskingPrice stockId={stockId} stockType={stockType} />}
+                            {option === 'detailInfo' && <StockDetailInfo/>}
+                        </div>
+                    </>
+                )}
             </div>
 
             <StockSpecBottomNavBar handleOrderSelect={handleOrderSelect}/>

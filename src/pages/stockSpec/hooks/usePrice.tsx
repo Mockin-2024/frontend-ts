0de7f1f92beccf699b03cc70b33d601289eaf77f
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-19 13:46:24 +0900

    feat: 주식 현재가 얻는 훅 추가

---
+++ src/pages/stockSpec/hooks/usePrice.tsx
@@ -0,0 +1,26 @@
+import useRestAPI from "../../../hooks/useRestAPI";
+
+import {Http} from "../../../enums/Http";
+import {Endpoint} from "../../../enums/Endpoint";
+import {StockType} from "../../../enums/StockType";
+import {convertStockTypeToExchangeCode} from "../../../utils/convertStockTypeToExchangeCode";
+
+const usePrice = (stockType: StockType, stockId: string) => {
+    const {data, loading, error} = useRestAPI(Endpoint.GET_PRICE_DETAIL, Http.GET, {
+        AUTH: "",
+        EXCD: convertStockTypeToExchangeCode(stockType),
+        SYMB: stockId
+    })
+    if (loading) {
+        return { priceInfo: null, loading: true, error: null };
+    }
+
+    if (error) {
+        return { priceInfo: null, loading: false, error };
+    }
+
+    const priceInfo = data.output;
+    return { priceInfo, loading: false, error: null };
+}
+
+export default usePrice

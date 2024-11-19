48e11fc1b2c648f3fef2d133e60b2cf89ae1e583
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-19 21:21:04 +0900

    refactor: 주식 상세 페이지, 숫자 포맷 개선

--- src/pages/stockSpec/components/StockDetailInfo.tsx
+++ src/pages/stockSpec/components/StockDetailInfo.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {PriceInfo} from "../type/PriceInfo";
+import {useCurrency} from "../../../context/CurrencyContext";
+import {formatNumber} from "../../../utils/formatNumber";
+import {formatCurrency} from "../../../utils/formatCurrency";
 
 interface Props{
     priceInfo: PriceInfo;
 }
 const StockDetailInfo: React.FC<Props> = ({priceInfo}) => {
+    const {convertToKRW} = useCurrency()
     return (
         <div>
             <div>
@@ -14,21 +18,21 @@ const StockDetailInfo: React.FC<Props> = ({priceInfo}) => {
                 <div className={"flex flex-col w-full pr-2 border-r border-r-gray-300"}>
                     <div className={"flex flex-row justify-between"}>
                         <div>시작가</div>
-                        <div>{priceInfo.open}{priceInfo.curr}</div>
+                        <div>{formatNumber(convertToKRW(Number(priceInfo.open), priceInfo.curr))}원</div>
                     </div>
                     <div className={"flex flex-row justify-between"}>
                         <div>종가</div>
-                        <div>{priceInfo.last}{priceInfo.curr}</div>
+                        <div>{formatNumber(convertToKRW(Number(priceInfo.last), priceInfo.curr))}원</div>
                     </div>
                 </div>
                 <div className={"flex flex-col w-full pl-2"}>
                     <div className={"flex flex-row justify-between"}>
                         <div>거래량</div>
-                        <div>{priceInfo.tvol}주</div>
+                        <div>{formatCurrency(Number(priceInfo.tvol))}주</div>
                     </div>
                     <div className={"flex flex-row justify-between"}>
                         <div>거래대금</div>
-                        <div>{priceInfo.tamt}{priceInfo.curr}</div>
+                        <div>{formatCurrency(convertToKRW(Number(priceInfo.tamt), priceInfo.curr))}원</div>
                     </div>
                 </div>
             </div>

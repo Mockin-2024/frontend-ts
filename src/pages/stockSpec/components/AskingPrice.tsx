c19aff15596f781c72172e08446a756bd7e8e1a8
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-19 14:36:28 +0900

    feat: 호가 컴포넌트 적용
    B
    호가 정보를 통해 총 10개의 호가 정보를 만들어줌.

--- src/pages/stockSpec/components/AskingPrice.tsx
+++ src/pages/stockSpec/components/AskingPrice.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import AskingPriceItem from "../../../components/item/AskingPriceItem";
+import {StockType} from "../../../enums/StockType";
+import {AskingPriceInfo} from "../type/AskingPriceInfo";
 
 interface Props {
-    stockType: string;
-    stockId: string;
+    askingPriceInfo: AskingPriceInfo
 }
 
-const AskingPrice:React.FC<Props> = ({stockType, stockId}) => {
+const AskingPrice:React.FC<Props> = ({askingPriceInfo}) => {
+
     return (
         <div>
-            <AskingPriceItem sellPrice={"100000"} buyVolume={"1"} sellVolume={"1"} sellChange={"1.5"}/>
+            {Array.from({ length: 10 }, (_, index) => {
+                const i = index + 1; // 1부터 10까지의 인덱스
+                return (
+                    <AskingPriceItem
+                        key={i}
+                        sellPrice={askingPriceInfo[`pask${i}` as keyof AskingPriceInfo]}
+                        buyVolume={askingPriceInfo[`vbid${i}` as keyof AskingPriceInfo]}
+                        sellVolume={askingPriceInfo[`vask${i}` as keyof AskingPriceInfo]}
+                        sellChange={askingPriceInfo[`dask${i}` as keyof AskingPriceInfo]}
+                    />
+                );
+            })}
         </div>
     )
 }

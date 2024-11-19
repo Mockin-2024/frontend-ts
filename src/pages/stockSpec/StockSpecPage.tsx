17c956577c63144a8828b0728aa48098d967d320
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-19 13:47:16 +0900

    feat: 현재가 훅 적용하여 주식 가격 정상 표시

--- src/pages/stockSpec/StockSpecPage.tsx
+++ src/pages/stockSpec/StockSpecPage.tsx
@@ -10,14 +10,16 @@ import {useLocation} from "react-router-dom";
 import useSelect from "./hooks/useSelect";
 import useSearchInfo from "./hooks/useSearchInfo";
 import LoadingOrError from "../../components/common/LoadingOrError";
+import usePrice from "./hooks/usePrice";
 
 const StockSpecPage: React.FC = () => {
-    const {stockType, stockId} = useLocation().state || {};
-    const {option, handleOptionSelect, handleOrderSelect} = useSelect(stockId)
-    const { searchInfo, loading: loadingSearchInfo, error: errorSearchInfo } = useSearchInfo(stockType, stockId);
+    const { stockType, stockId } = useLocation().state || {};
+    const { option, handleOptionSelect, handleOrderSelect } = useSelect(stockId)
+    const { searchInfo, loading: loadingSearchInfo, error: errorSearchInfo } = useSearchInfo(stockType, stockId)
+    const { priceInfo, loading: loadingPrice, error: errorPrice } = usePrice(stockType, stockId)
 
-    const isLoading = loadingSearchInfo;
-    const isError = errorSearchInfo;
+    const isLoading = loadingSearchInfo || loadingPrice;
+    const isError = errorSearchInfo || errorPrice;
 
     return (
         <div className={""}>
@@ -27,7 +29,12 @@ const StockSpecPage: React.FC = () => {
             <div className={"pt-20 p-4"}>
                 {!isLoading && !isError && searchInfo && (
                     <>
-                        <StockInfo name={searchInfo.prdt_name} price={"100000"} type={searchInfo.ovrs_excg_name}/>
+                        <StockInfo
+                            name={searchInfo.prdt_name}
+                            price={priceInfo.last}
+                            type={searchInfo.ovrs_excg_name}
+                            currency={priceInfo.curr}
+                        />
                         <div className={"flex p-4 justify-between "}>
                             <div onClick={() => handleOptionSelect('chart')}>차트</div>
                             <div onClick={() => handleOptionSelect('askingPrice')}>호가</div>

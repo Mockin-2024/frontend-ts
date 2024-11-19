87aceef52de7616348165b4d4eeb9bb8361e033d
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-19 21:19:42 +0900

    refactor: 숫자 표시시 소숫점 삭제

--- src/utils/formatCurrency.tsx
+++ src/utils/formatCurrency.tsx
@@ -1,12 +1,12 @@
 export const formatCurrency = (num: number): string => {
     if (num >= 1e16) {
-        return (num / 1e16).toFixed(2) + '경';
+        return Math.floor(num / 1e16) + '경';
     } else if (num >= 1e12) {
-        return (num / 1e12).toFixed(2) + '조';
+        return Math.floor(num / 1e12) + '조';
     } else if (num >= 1e8) {
-        return (num / 1e8).toFixed(2) + '억';
+        return Math.floor(num / 1e8) + '억';
     } else if (num >= 1e4) {
-        return (num / 1e4).toFixed(2) + '만';
+        return Math.floor(num / 1e4) + '만';
     } else {
         return num.toString();
     }

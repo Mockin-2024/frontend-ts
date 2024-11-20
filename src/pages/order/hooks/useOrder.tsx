e2cda84bfb3bf753149dd2c727a42fc662a2b21a
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-20 17:48:56 +0900

    feat: 주문 요청 훅 추가

---
+++ src/pages/order/hooks/useOrder.tsx
@@ -0,0 +1,42 @@
+import apiRequest from "../../../utils/apiRequest";
+import Alert from "../../../components/common/Alert";
+
+import {Endpoint} from "../../../enums/Endpoint";
+import {Http} from "../../../enums/Http";
+import {AlertIcon} from "../../../enums/AlertIcon";
+import {ExchangeCode} from "../../../enums/ExchangeCode";
+import {OrderType} from "../../../enums/OrderType";
+
+import {toOrderTransactionId} from "../../../utils/toOrderTransactionId";
+import {useCurrency} from "../../../context/CurrencyContext";
+
+export interface OrderHandler {
+    (orderType: OrderType, quantity: string, price: string): Promise<void>;
+}
+
+const useOrder = (exchangeCode: ExchangeCode, stockId: string,) => {
+    const {convertFromKRW} = useCurrency()
+    const orderHandler: OrderHandler = async (
+        orderType: OrderType,
+        quantity: string,
+        price: string
+        ) => {
+        const {data, error} = await apiRequest(Endpoint.POST_ORDER, Http.POST, {
+            transactionId: toOrderTransactionId(exchangeCode, orderType),
+            overseasExchangeCode: exchangeCode,
+            productNumber: stockId,
+            orderQuantity: quantity,
+            overseasOrderUnitPrice:price
+        })
+
+        if (error){
+            await Alert(`${orderType} 실패`, error.toString(), AlertIcon.ERROR)
+            return;
+        }
+        console.log(data)
+        await Alert(`${orderType} 성공`, ` `, AlertIcon.ERROR)
+    }
+    return {orderHandler}
+}
+
+export default useOrder

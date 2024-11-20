63e0cf2b241238526fafe117e8c913fad647b18b
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-20 17:51:33 +0900

    refactor: 주문 확인 모달창 켜지도록 적용

--- src/pages/order/components/OrderButton.tsx
+++ src/pages/order/components/OrderButton.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import {OrderType} from "../../../enums/OrderType";
 
 interface Props {
-    orderType: string;
+    orderType: OrderType;
+    openModal: () => void;
 }
 
-const OrderButton: React.FC<Props> = ({orderType}) => {
+const OrderButton: React.FC<Props> = ({orderType, openModal}) => {
     return (
         <div className={"fixed bottom-0 left-0 right-0 flex p-4"}>
-            <div className={`rounded-2xl text-white px-10 py-4 w-full text-center mx-2 ${orderType === "판매" ? "bg-blue-600" : "bg-red-500"}`}>
+            <div className={`rounded-2xl text-white px-10 py-4 w-full text-center mx-2 ${orderType === "판매" ? "bg-blue-600" : "bg-red-500"}`}
+            onClick={openModal}>
                 {orderType}하기
             </div>
         </div>

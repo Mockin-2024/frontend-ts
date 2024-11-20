b1533c6bb78e730ea7bf31e5a2144c3721509c18
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-20 14:32:01 +0900

    refactor: 상세 -> 매매 페이지 state 개선
    state에 환율도 함께 넣어 보냄.

--- src/pages/stockSpec/components/StockSpecBottomNavBar.tsx
+++ src/pages/stockSpec/components/StockSpecBottomNavBar.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import {toExchangeCode} from "../../../utils/toExchangeCode";
 import {ExchangeCode} from "../../../enums/ExchangeCode";
+import {OrderType} from "../../../enums/OrderType";
 
-type HandleOrderSelectType = (stockName:string, orderType: string, exchangeCode:ExchangeCode, price:string) => void;
+type HandleOrderSelectType = (stockName:string, orderType: OrderType, exchangeCode:ExchangeCode, price:string, curr:string) => void;
 
 interface Props{
     handleOrderSelect: HandleOrderSelectType;
     exchangeCode: string;
     price: string;
     stockName: string;
+    curr: string;
 }
 
-const StockSpecBottomNavBar: React.FC<Props> = ({stockName, handleOrderSelect, exchangeCode, price}) => {
+const StockSpecBottomNavBar: React.FC<Props> = ({stockName, handleOrderSelect, exchangeCode, price, curr}) => {
     return (
         <div className={"fixed bottom-0 left-0 right-0 flex justify-between p-4"}>
             <div className={"rounded-2xl bg-blue-600 text-white px-10 py-4 w-full text-center mx-2"}
-                 onClick={(e) => handleOrderSelect(stockName, "판매", toExchangeCode(exchangeCode), price)}>
+                 onClick={(e) => handleOrderSelect(stockName, OrderType.SELL, toExchangeCode(exchangeCode), price, curr)}>
                 판매하기
             </div>
             <div className={"rounded-2xl bg-red-500 text-white px-10 py-4 w-full text-center mx-2"}
-                 onClick={(e) => handleOrderSelect(stockName, "구매", toExchangeCode(exchangeCode), price)}>
+                 onClick={(e) => handleOrderSelect(stockName, OrderType.BUY, toExchangeCode(exchangeCode), price, curr)}>
                 구매하기
             </div>
         </div>
--- src/pages/stockSpec/hooks/useSelect.tsx
+++ src/pages/stockSpec/hooks/useSelect.tsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import useNavigator from "../../../hooks/useNavigator";
 import {AppRoutes} from "../../../enums/AppRoutes";
 import {ExchangeCode} from "../../../enums/ExchangeCode";
+import {OrderType} from "../../../enums/OrderType";
 
 const useSelect = (stockId:string) => {
     const [option, setOption] = useState<string | null>("chart");
@@ -11,13 +12,19 @@ const useSelect = (stockId:string) => {
         setOption(option);
     };
 
-    const handleOrderSelect = (stockName: string, orderType: string, exchangeCode: ExchangeCode, price: string) => {
+    const handleOrderSelect = (
+        stockName: string,
+        orderType: OrderType,
+        exchangeCode: ExchangeCode,
+        price: string,
+        curr: string) => {
         navigateTo(AppRoutes.ORDER, {
             orderType: orderType,
             stockId: stockId,
             price: price,
             exchangeCode: exchangeCode,
-            stockName:stockName
+            stockName:stockName,
+            curr:curr
         })
     }
 

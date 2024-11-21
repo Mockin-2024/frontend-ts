f63bd652165513e35baf3e1f34ff786e77e7f314
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-21 17:37:08 +0900

    feat: 체크 아이콘 추가

---
+++ src/components/Icon/CheckYes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { IoCheckmarkCircle } from "react-icons/io5";
+
+const CheckYes:React.FC = () => {
+    return(
+        <div>
+            <IoCheckmarkCircle/>
+        </div>
+    )
+}
+
+export default CheckYes;

266210b182d7e8b352587696b85c5aae9ee14005
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-21 17:37:23 +0900

    feat: 거래소 한글 enum 추가

---
+++ src/enums/ExchangeCodeKor.tsx
@@ -0,0 +1,11 @@
+export enum ExchangeCodeKor {
+    NAS = "나스닥",
+    NYS = "뉴욕",
+    AMS = "아멕스",
+    HKS = "홍콩",
+    SHS = "상해" ,
+    SZS = "심천",
+    HSX = "호치민",
+    HNX = "하노이",
+    TSE = "도쿄"
+}

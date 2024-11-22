868b784511ccbc7ccbf977d959c04578b61c15c4
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-11-22 16:32:00 +0900

    refactor: 활용하지 않는 import 문 삭제

--- src/pages/search/components/SearchOption.tsx
+++ src/pages/search/components/SearchOption.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent} from "react";
 import CheckNo from "../../../components/Icon/CheckNo";
 import CheckYes from "../../../components/Icon/CheckYes";
 
